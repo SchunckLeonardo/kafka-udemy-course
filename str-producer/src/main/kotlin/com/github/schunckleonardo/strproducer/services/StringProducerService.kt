7a5f04b9e9a9982ea920cac87c8cb72606eb5c43
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2026-02-04 06:28:36 -0300

    feat: add callback function to see results

--- str-producer/src/main/kotlin/com/github/schunckleonardo/strproducer/services/StringProducerService.kt
+++ str-producer/src/main/kotlin/com/github/schunckleonardo/strproducer/services/StringProducerService.kt
@@ -1,18 +1,31 @@
 package com.github.schunckleonardo.strproducer.services
 
 import com.github.schunckleonardo.strproducer.entities.constants.KafkaTopicConstants
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class StringProducerService(
-    private val kafkaTemplate: KafkaTemplate<String, String>
+    private val kafkaTemplate: KafkaTemplate<String, String>,
+    private val logger: Logger = LoggerFactory.getLogger(StringProducerService::class.java)
 ) {
 
     fun sendMessage(message: String) {
-        kafkaTemplate.send(
+        sendMessageWithKey(
             KafkaTopicConstants.STR_TOPIC, message
+        )  { success ->
+            if (!success) logger.error("Failed to send message to topic: ${KafkaTopicConstants.STR_TOPIC}")
+        }
+    }
+
+    private fun sendMessageWithKey(topic: String, message: String, response: (success: Boolean) -> Unit) {
+        val sendResult = kafkaTemplate.send(
+            topic, message
         )
+
+        if (sendResult.isCompletedExceptionally) response(false) else response(true)
     }
 
 }
