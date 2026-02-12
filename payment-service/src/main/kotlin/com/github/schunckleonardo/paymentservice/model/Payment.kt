04c21c58e4e067594fef6247558a3e0a87aa908f
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2026-02-12 07:05:42 -0300

    feat: add JSON producer configuration and integrate Kafka for payment processing

--- payment-service/src/main/kotlin/com/github/schunckleonardo/paymentservice/model/Payment.kt
+++ payment-service/src/main/kotlin/com/github/schunckleonardo/paymentservice/model/Payment.kt
@@ -1,8 +1,10 @@
 package com.github.schunckleonardo.paymentservice.model
 
+import java.io.Serializable
+
 data class Payment(
     val id: Long,
     val idUser: Long,
     val idProduct: Long,
     val cardNumber: String
-)
+) : Serializable
--- payment-service/src/main/kotlin/com/github/schunckleonardo/paymentservice/service/impl/PaymentServiceImpl.kt
+++ payment-service/src/main/kotlin/com/github/schunckleonardo/paymentservice/service/impl/PaymentServiceImpl.kt
@@ -4,15 +4,22 @@ import com.github.schunckleonardo.paymentservice.model.Payment
 import com.github.schunckleonardo.paymentservice.service.PaymentService
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
+import java.io.Serializable
 
 @Service
 class PaymentServiceImpl(
-    private val logger: Logger = LoggerFactory.getLogger(PaymentServiceImpl::class.java)
+    private val logger: Logger = LoggerFactory.getLogger(PaymentServiceImpl::class.java),
+    private val kafkaTemplate: KafkaTemplate<String, Serializable>
 ) : PaymentService {
 
     override fun sendPayment(payment: Payment) {
         logger.info("[PaymentServiceImpl][sendPayment] ::: Sending payment: $payment")
+        Thread.sleep(1000)
+
+        logger.info("[PaymentServiceImpl][sendPayment] ::: Payment sent successfully")
+        kafkaTemplate.send("payment-topic", payment)
     }
 
 }
