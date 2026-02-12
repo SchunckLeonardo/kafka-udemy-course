9740e2e6bca6ced16d846c8ea66a34b68de6be7d
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2026-02-12 06:02:51 -0300

    feat: add custom interceptor

--- str-consumer/src/main/kotlin/com/github/schunckleonardo/strconsumer/config/StringConsumerConfig.kt
+++ str-consumer/src/main/kotlin/com/github/schunckleonardo/strconsumer/config/StringConsumerConfig.kt
@@ -2,17 +2,22 @@ package com.github.schunckleonardo.strconsumer.config
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.RecordInterceptor
 
 @Configuration
 class StringConsumerConfig(
     private val kafkaProperties: KafkaProperties
 ) {
+
+    private val logger: Logger = LoggerFactory.getLogger(StringConsumerConfig::class.java)
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, String> {
@@ -29,5 +34,23 @@ class StringConsumerConfig(
         ConcurrentKafkaListenerContainerFactory<String, String>().apply {
             this.consumerFactory = consumerFactory
         }
+
+    @Bean
+    fun validMessageContainerFactory(consumerFactory: ConsumerFactory<String, String>): ConcurrentKafkaListenerContainerFactory<String, String> =
+        ConcurrentKafkaListenerContainerFactory<String, String>().apply {
+            this.consumerFactory = consumerFactory
+            this.setRecordInterceptor(validMessage())
+        }
+
+    private fun validMessage(): RecordInterceptor<String, String> {
+        return RecordInterceptor { record, _ ->
+            if (record.value().contains("Test")) {
+                logger.info("Has the word 'Test', processing message: ${record.value()}")
+                record
+            } else {
+                record
+            }
+        }
+    }
 
 }
--- str-consumer/src/main/kotlin/com/github/schunckleonardo/strconsumer/listeners/StrConsumerListener.kt
+++ str-consumer/src/main/kotlin/com/github/schunckleonardo/strconsumer/listeners/StrConsumerListener.kt
@@ -3,6 +3,7 @@ package com.github.schunckleonardo.strconsumer.listeners
 import com.github.schunckleonardo.strconsumer.listeners.custom.StrConsumerCustomListener
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,9 +11,23 @@ class StrConsumerListener(
     private val logger: Logger = LoggerFactory.getLogger(StrConsumerListener::class.java)
 ) {
 
+    @KafkaListener(
+        groupId = "group-1",
+        topics = ["str-topic"],
+        containerFactory = "concurrentKafkaListenerContainerFactory"
+    )
     @StrConsumerCustomListener(groupId = "group-1")
     fun create(message: String) {
         logger.info("CREATE ::: Received message: $message")
+    }
+
+    @KafkaListener(
+        groupId = "group-2",
+        topics = ["str-topic"],
+        containerFactory = "validMessageContainerFactory"
+    )
+    fun history(message: String) {
+        logger.info("HISTORY ::: Received message: $message")
     }
 
 }
